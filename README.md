78832acdbf1b2267f1604361b627182071ed6c29
Lauren <lmsullivan18@gmail.com>
2021-02-21 13:15:17 -0500

    Updating with challenge code.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # UFOs
+
+## Overview
+
+The purpose of this assignment was to update our UFO sightings websites to allow users to filter for multiple criteria at the same time. While the original page only allowed you to filter on date, the new page also allows filters on city, state, country and shape.
+
+## Results
+
+To use the new webpage, all a user must do is enter a new search criterion in the corresponding search bar. For example, a user can find all UFO sightings that took place in California by typing “ca” into the “Enter State” search bar.
+
+<<ADD IMAGES >>
+
+## Summary
+
+One drawback of the webpage is the lack of the filter button – I personally would add that back in as I think it makes the user experience a little bit better. My two recommendations for further development would be to add back the filter button, and use that as the “event” for filtering the table. I would also 
+
+
--- static/js/ufo_starterCode.js
+++ static/js/ufo_starterCode.js
@@ -35,38 +35,35 @@ function updateFilters() {
     let elementValue = changedElement.property("value")
     console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
-    let filterId = elementValue.attr("id")
-    console.log(filterID);
+    let filterId = changedElement.attr("id")
+    console.log(filterId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
-  if (elementValue) {
+    if (elementValue) {
     filters[filterId] = elementValue;
   }
-  else {
+    else {
     delete filters[filterID];
   }
   
     // 6. Call function to apply all filters and rebuild the table
-    updateFilters();
-  
+    filterTable();
   }
   
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-   let filteredData = tableData;
-  
+    let filteredData = tableData;
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-   if (elementValue) [
-   filteredData = filteredData.filter(row => row.changedElement === "id")
-   ]
+Object.entries(filters).forEach(([key, value]) => {
+   filteredData = filteredData.filter(row => row[key] === value)
+})
   
     // 10. Finally, rebuild the table using the filtered data
-  filterTable(filteredData)
- }
+  buildTable(filteredData)};
   
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
